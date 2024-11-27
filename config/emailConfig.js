f7492edca7ceaf14737afea597fa734b038aba3c
sandip kankhare <sandipkankhare940@gmail.com>
2024-11-27 16:17:04 +0530

    change the mail

--- config/emailConfig.js
+++ config/emailConfig.js
@@ -1,8 +1,16 @@
-module.exports = {
+// module.exports = {
+//     service: 'gmail',
+//     auth: {
+//       user: 'kankharesandip78@gmail.com',
+//       pass: 'dgpt dinj wjdv fhpc',
+//     },
+//   };
+  
+
+  module.exports = {
     service: 'gmail',
     auth: {
-      user: 'kankharesandip78@gmail.com',
-      pass: 'dgpt dinj wjdv fhpc',
+      user: 'projectfinalcse1@gmail.com',
+      pass: 'vyzp njdn mauz nlti',
     },
   };
-  
